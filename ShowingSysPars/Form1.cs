46ce4097f68271e4a55621cfb7e0974eda126d10
dygy <yukimurachris@gmail.com>
2019-04-13 20:33:38 +0300

    Добавьте файлы проекта.

---
+++ ShowingSysPars/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowingSysPars
+{
+    public partial class Form1 : Form
+    {
+        private ulong ram;
+        private ulong availExtendedVirtual;
+        private ulong availPageFile;
+        private ulong availPhys;
+        private ulong availVirtual;
+
+        public Form1(ulong ram, ulong availExtendedVirtual, 
+            ulong availPageFile, 
+            ulong availPhys, ulong availVirtual,
+            uint memoryLoad, ulong totalPageFile, 
+            ulong totalPhys, ulong totalVirtual, uint dwLength)
+        {
+            this.ram = ram;
+            this.availExtendedVirtual = availExtendedVirtual;
+            this.availPageFile = availPageFile;
+            this.availPhys = availPhys;
+            this.availVirtual = availVirtual;
+            InitializeComponent();
+            label1.Text = "Randpme Access Memory " + ram / 1024 / 1024;
+            label2.Text = "Файл подкачки " + availExtendedVirtual;
+            label3.Text = "Свободных байт в файле подкачки " + availPageFile;
+            label4.Text = "Свободная физическая память, байт " + availPhys;
+            label5.Text = "Свободная виртуальная память " + availVirtual;
+            label6.Text = "Процент использования памяти " + memoryLoad;
+            label7.Text = "Размер файла подкачки, байт " + totalPageFile;
+            label8.Text = "Физическая память, байт  " + totalPhys;
+            label9.Text = "Виртуальная память, используемая процессом " + totalVirtual;
+            label10.Text = "Размер структуры " + dwLength ;
+
+            /*
+            DWORD dwLength;     // Размер структуры
+            DWORD dwMemoryLoad; // Процент использования памяти
+            DWORD dwTotalPhys;  // Физическая память, байт
+            DWORD dwAvailPhys;  // Свободная физическая память, байт
+            DWORD dwTotalPageFile;  // Размер файла подкачки, байт
+            DWORD dwAvailPageFile;  // Свободных байт в файле подкачки
+            DWORD dwTotalVirtual;   // Виртуальная память, используемая процессом 
+            DWORD dwAvailVirtual;   // Свободная виртуальная память
+            */
+
+
+        }
+    }
+}
---
+++ ShowingSysPars/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowingSysPars
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    namespace ConsoleApplication39
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MemoryStatus
+        {
+            [return: MarshalAs(UnmanagedType.Bool)]
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            internal static extern bool GlobalMemoryStatusEx([In, Out] MemoryStatus lpBuffer);
+
+            public uint dwLength;
+            public uint MemoryLoad;
+            public ulong TotalPhys;
+            public ulong AvailPhys;
+            public ulong TotalPageFile;
+            public ulong AvailPageFile;
+            public ulong TotalVirtual;
+            public ulong AvailVirtual;
+            public ulong AvailExtendedVirtual;
+
+            private static volatile MemoryStatus singleton;
+            private static readonly object syncroot = new object();
+
+            public static MemoryStatus CreateInstance()
+            {
+                if (singleton == null)
+                    lock (syncroot)
+                        if (singleton == null)
+                            singleton = new MemoryStatus();
+                return singleton;
+            }
+
+            [SecurityCritical]
+            private MemoryStatus()
+            {
+                dwLength = (uint)Marshal.SizeOf(typeof(MemoryStatus));
+                GlobalMemoryStatusEx(this);
+            }
+        }
+
+        class Program
+        {
+
+            static void Main()
+            {
+                MemoryStatus status = MemoryStatus.CreateInstance();
+                ulong ram = status.TotalPhys;
+                Console.WriteLine("RAM = {0} MB", ram / 1024 / 1024);
+                //Console.ReadKey();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1(ram,status.AvailExtendedVirtual,
+                    status.AvailPageFile, status.AvailPhys,
+                    status.AvailVirtual, status.MemoryLoad ,
+                    status.TotalPageFile,status.TotalPhys,
+                    status.TotalVirtual,status.dwLength
+
+                ));
+
+            }
+        }
+    }
+   
+}
